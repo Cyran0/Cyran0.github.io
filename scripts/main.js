1b5704077cc99672d9a157175e1b9265e20e00b8
Cyrano <bourgouinevan@gmail.com>
2018-09-28 00:50:20 +0200

    Deuxieme commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    setTimeout(function(){ 
+    var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");       
+       if( r == true) {
+           window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+                   }
+    }, 10000);
+};
